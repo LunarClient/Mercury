64ed97777995324bcc19efe22f79d1987a704916
Colin McDonald <colin@colinmcdonald.me>
2025-05-14 16:27:10 -0400

    chore: update to new Maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -198,7 +198,7 @@ publishing {
 
     repositories {
         maven {
-            url = uri("artifactregistry://us-maven.pkg.dev/moonsworth-299m4oir/maven-public")
+            url = uri("artifactregistry://us-maven.pkg.dev/mw-lunarclient-maven-repo/public")
         }
     }
 }
